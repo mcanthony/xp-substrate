ecf77f0cf7190d8a625e30b992f03e27f0270ca5
Damien Mortini <ddotmnn+git@gmail.com>
2015-05-14 17:48:17 +0100

    quick test with 2d render

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,16 +8,23 @@ class XPSubstrateElement extends LoopElement {
   createdCallback() {
     super.createdCallback();
 
-    this.canvas = this.shadowRoot.querySelector("canvas");
+
+    this.canvas = this.shadowRoot.querySelector("canvas#roofs");
     this.canvas.width = this.offsetWidth;
     this.canvas.height = this.offsetHeight;
-
     this.context = this.canvas.getContext("2d");
+
+    this.canvasGround = this.shadowRoot.querySelector("canvas#ground");
+    this.canvasGround.width = this.canvas.width;
+    this.canvasGround.height = this.canvas.height;
+    this.contextGround = this.canvasGround.getContext("2d");
+
+    this.buildings = [];
 
     this.substrateSystem = new SubstrateSystem(this.canvas.width, this.canvas.height, {
       speed: 4,
       // spawnProbabilityRatio: 0
-      spawnProbabilityRatio: 0.05
+      spawnProbabilityRatio: 0.01
     });
 
     this.substrateDebugRenderer = new SubstrateDebugRenderer(this.substrateSystem, {
@@ -32,17 +39,23 @@ class XPSubstrateElement extends LoopElement {
 
     this.update();
 
-    // this.substrateSystem.spawnEdge(this.canvas.width * 0.5, this.canvas.height * 0.5, Math.PI * .5);
+    this.substrateSystem.spawnEdge(this.canvas.width * 0.5, this.canvas.height * 0.5, Math.PI * .5);
 
     this.addEventListener("mousedown", this);
     this.addEventListener("mouseup", this);
+    this.addEventListener("mousemove", this);
   }
 
-  handleEvent (e) {
+  handleEvent(e) {
     super.handleEvent(e);
     switch (e.type) {
       case "mousedown":
         this.pointerEdge.a.set(e.clientX, e.clientY);
+        break;
+      case "mousemove":
+        this.pointerEdge.a.copy(this.pointerEdge.b);
+        this.pointerEdge.b.set(e.clientX / this.offsetWidth, e.clientY / this.offsetHeight).multiplyScalar(2).addScalar(-1);
+        // console.log(this.pointerEdge.b);
         break;
       case "mouseup":
         this.pointerEdge.b.set(e.clientX, e.clientY);
@@ -55,21 +68,39 @@ class XPSubstrateElement extends LoopElement {
     // if(polygon.vertices.length > 4) {
     //   return;
     // }
-    this.context.fillStyle = `hsl(${360 * Math.random()}, 100%, 80%)`;
-    this.context.beginPath();
-    let vertex = polygon.vertices[0];
-    this.context.moveTo(vertex.x, vertex.y);
-    for (let i = 1; i < polygon.vertices.length; i++) {
-      vertex = polygon.vertices[i];
-      this.context.lineTo(vertex.x, vertex.y);
+    let building = {
+      polygon: polygon,
+      color: `hsl(${360 * Math.random()}, 100%, 80%)`,
+      height: Math.random()
+    };
+    this.buildings.push(building);
+    this.drawBuilding(building, this.contextGround);
+  }
+
+  drawBuilding(building, context) {
+    context.fillStyle = building.color;
+    context.beginPath();
+    let vertex = building.polygon.vertices[0];
+    context.moveTo(vertex.x, vertex.y);
+    for (let i = 1; i < building.polygon.vertices.length; i++) {
+      vertex = building.polygon.vertices[i];
+      context.lineTo(vertex.x, vertex.y);
     }
-    this.context.fill();
+    context.fill();
   }
 
   update() {
     super.update();
     this.substrateSystem.update();
-    this.substrateDebugRenderer.update();
+
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    for (let building of this.buildings) {
+      this.context.save();
+      this.context.translate(this.pointerEdge.b.x * 20 * building.height, this.pointerEdge.b.y * 20 * building.height);
+      this.drawBuilding(building, this.context);
+      this.context.restore();
+    }
+    // this.substrateDebugRenderer.update();
   }
 }
 
