d153a9fd57d870daca2f98c3dcbe279053da0c4f
Damien Mortini <ddotmnn+git@gmail.com>
2015-05-14 16:38:32 +0100

    add boid at drag and drop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,30 +1,71 @@
 import LoopElement from "../../bower_components/dlib/dom/LoopElement";
 import SubstrateSystem from "../../bower_components/dlib/substrate/SubstrateSystem";
 import SubstrateDebugRenderer from "../../bower_components/dlib/substrate/SubstrateDebugRenderer";
+import Edge from "../../bower_components/dlib/math/Edge";
 
 class XPSubstrateElement extends LoopElement {
+
   createdCallback() {
     super.createdCallback();
 
-    let canvas = this.shadowRoot.querySelector("canvas");
-    canvas.width = this.offsetWidth;
-    canvas.height = this.offsetHeight;
+    this.canvas = this.shadowRoot.querySelector("canvas");
+    this.canvas.width = this.offsetWidth;
+    this.canvas.height = this.offsetHeight;
 
-    this.substrateSystem = new SubstrateSystem(canvas.width, canvas.height, {
-      speed: 10,
+    this.context = this.canvas.getContext("2d");
+
+    this.substrateSystem = new SubstrateSystem(this.canvas.width, this.canvas.height, {
+      speed: 4,
+      // spawnProbabilityRatio: 0
       spawnProbabilityRatio: 0.05
     });
 
     this.substrateDebugRenderer = new SubstrateDebugRenderer(this.substrateSystem, {
-      canvas: canvas,
-      edgesDebug: false,
-      polygonsDebug: false
+      canvas: this.shadowRoot.querySelector("canvas#debug"),
+      // edgesDebug: true,
+      // polygonsDebug: true
     });
+
+    this.pointerEdge = new Edge();
+
+    this.substrateSystem.polygonAddedCallback = this.polygonAdded.bind(this);
 
     this.update();
 
-    this.substrateSystem.addBoid(canvas.width * 0.5, canvas.height * 0.5, Math.PI);
+    // this.substrateSystem.spawnEdge(this.canvas.width * 0.5, this.canvas.height * 0.5, Math.PI * .5);
+
+    this.addEventListener("mousedown", this);
+    this.addEventListener("mouseup", this);
   }
+
+  handleEvent (e) {
+    super.handleEvent(e);
+    switch (e.type) {
+      case "mousedown":
+        this.pointerEdge.a.set(e.clientX, e.clientY);
+        break;
+      case "mouseup":
+        this.pointerEdge.b.set(e.clientX, e.clientY);
+        this.substrateSystem.spawnEdge(this.pointerEdge.a.x, this.pointerEdge.a.y, this.pointerEdge.angle);
+        break;
+    }
+  }
+
+  polygonAdded(polygon) {
+    // if(polygon.vertices.length > 4) {
+    //   return;
+    // }
+    this.context.fillStyle = `hsl(${360 * Math.random()}, 100%, 80%)`;
+    this.context.beginPath();
+    let vertex = polygon.vertices[0];
+    this.context.moveTo(vertex.x, vertex.y);
+    for (let i = 1; i < polygon.vertices.length; i++) {
+      vertex = polygon.vertices[i];
+      this.context.lineTo(vertex.x, vertex.y);
+    }
+    this.context.fill();
+  }
+
   update() {
     super.update();
     this.substrateSystem.update();
